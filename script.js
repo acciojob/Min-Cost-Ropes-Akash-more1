236adb2bbe708cfcbb6f4e71f625d6fd33e68243
Akash More <122983542+Akash-more1@users.noreply.github.com>
2024-06-03 19:08:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+let n=arr.length;
+
+	if(n==1){
+		return 0;
+	}
+	else if(n==2){
+		return arr[0]+arr[1];
+	}
+	else{
+		arr.sort();
+			
+		let sum1=arr[0]+arr[1];
+		let sum2=arr[0]+arr[1];
+
+		for(let i=1; i<arr.length-1; i++){
+		  sum2=sum2+arr[i+1];
+		  sum1+=sum2;
+		}
+	
+		return sum1;
+	}
   
 }
 
